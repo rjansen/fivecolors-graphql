c2f76eb4d42aa22402eadf3e1751259f6fd20759
Raffael Tancman <rtancman@gmail.com>
2019-07-29 14:09:44 -0300

    Create app core

---
+++ core/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+from django.contrib.postgres import fields as pgfields
+import hashlib
+
+
+def sha1(v):
+    if v:
+        h = hashlib.sha1()
+        h.update(v.encode('utf-8'))
+        return h.hexdigest()
+
+
+class Set(models.Model):
+    id = models.CharField(primary_key=True, max_length=40)
+    name = models.CharField(blank=False, max_length=255)
+    alias = models.CharField(blank=False, max_length=15)
+    asset = pgfields.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+
+class Rarity():
+    COMMON = sha1('Rarity#Common')
+    UNCOMMON = sha1('Rarity#Uncommon')
+    RARE = sha1('Rarity#Rare')
+    MYTHIC_RARE = sha1('Rarity#Mythic Rare')
+    SPECIAL = sha1('Rarity#Special')
+    LAND = sha1('Rarity#Land')
+    PROMO = sha1('Rarity#Promo')
+    BONUS = sha1('Rarity#Bonus')
+
+    ALL = [
+        (COMMON, 'Common'),
+        (UNCOMMON, 'Unccommon'),
+        (RARE, 'Rare'),
+        (MYTHIC_RARE, 'Mythic Rare'),
+        (SPECIAL, 'Special'),
+        (LAND, 'Land'),
+        (PROMO, 'Promo'),
+        (BONUS, 'Bonus'),
+    ]
+
+
+class Card(models.Model):
+    id = models.CharField(primary_key=True, max_length=40)
+    name = models.CharField(blank=False, max_length=255)
+    types = pgfields.ArrayField(models.CharField(blank=False, max_length=128))
+    costs = pgfields.ArrayField(models.CharField(max_length=5))
+    number_cost = models.DecimalField(max_digits=14, decimal_places=4)
+    id_external = models.CharField(max_length=100)
+    id_rarity = models.CharField(
+        blank=False, choices=Rarity.ALL, max_length=40
+    )
+    # rarity = models.ForeignKey('Rarity', on_delete=models.PROTECT)
+    id_set = models.CharField(blank=False, max_length=40)
+    set = models.ForeignKey('Set', on_delete=models.PROTECT)
+    id_asset = models.CharField(blank=False, max_length=40)
+    rate = models.DecimalField(max_digits=14, decimal_places=4)
+    rate_votes = models.DecimalField(max_digits=10, decimal_places=0)
+    rules = pgfields.ArrayField(models.CharField(blank=False, max_length=512))
+    order_external = models.CharField(max_length=15)
+    artist = models.CharField(max_length=255)
+    flavor = models.TextField(max_length=1024)
+    data = pgfields.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
