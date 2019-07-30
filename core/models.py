ca2e282d5e18ac8ae1184c2d87bd90bd2c445a33
Raffael Tancman <rtancman@gmail.com>
2019-07-29 23:04:09 -0300

    Fix models

--- core/models.py
+++ core/models.py
@@ -12,12 +12,12 @@ def sha1(v):
 
 class Set(models.Model):
     id = models.CharField(primary_key=True, max_length=40)
-    name = models.CharField(blank=False, max_length=255)
-    alias = models.CharField(blank=False, max_length=15)
-    asset = pgfields.JSONField()
+    name = models.CharField(max_length=255)
+    alias = models.CharField(max_length=15)
+    asset = pgfields.JSONField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField()
+    updated_at = models.DateTimeField(null=True, auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
 
     def __str__(self):
         return self.name
@@ -47,28 +47,26 @@ class Rarity():
 
 class Card(models.Model):
     id = models.CharField(primary_key=True, max_length=40)
-    name = models.CharField(blank=False, max_length=255)
-    types = pgfields.ArrayField(models.CharField(blank=False, max_length=128))
-    costs = pgfields.ArrayField(models.CharField(max_length=5))
-    number_cost = models.DecimalField(max_digits=14, decimal_places=4)
+    name = models.CharField(max_length=255)
+    types = pgfields.ArrayField(models.CharField(max_length=128))
+    costs = pgfields.ArrayField(models.CharField(max_length=5), default=list)
+    number_cost = models.DecimalField(default=0, max_digits=14, decimal_places=4)
     id_external = models.CharField(max_length=100)
-    id_rarity = models.CharField(
-        blank=False, choices=Rarity.ALL, max_length=40
-    )
+    id_rarity = models.CharField(choices=Rarity.ALL, max_length=40)
     # rarity = models.ForeignKey('Rarity', on_delete=models.PROTECT)
-    id_set = models.CharField(blank=False, max_length=40)
-    set = models.ForeignKey('Set', on_delete=models.PROTECT)
-    id_asset = models.CharField(blank=False, max_length=40)
-    rate = models.DecimalField(max_digits=14, decimal_places=4)
-    rate_votes = models.DecimalField(max_digits=10, decimal_places=0)
-    rules = pgfields.ArrayField(models.CharField(blank=False, max_length=512))
-    order_external = models.CharField(max_length=15)
-    artist = models.CharField(max_length=255)
-    flavor = models.TextField(max_length=1024)
-    data = pgfields.JSONField()
+    # id_set = models.CharField(max_length=40)
+    set = models.ForeignKey('Set', on_delete=models.PROTECT, db_column='id_set', max_length=40)
+    id_asset = models.CharField(max_length=40)
+    rate = models.DecimalField(default=0, max_digits=14, decimal_places=4)
+    rate_votes = models.DecimalField(default=0, max_digits=10, decimal_places=0)
+    rules = pgfields.ArrayField(models.CharField(max_length=512), default=list)
+    order_external = models.CharField(null=True, max_length=15)
+    artist = models.CharField(null=True, max_length=255)
+    flavor = models.TextField(null=True, max_length=1024)
+    data = pgfields.JSONField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField()
+    updated_at = models.DateTimeField(null=True, auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
 
     def __str__(self):
         return self.name
